09227bd8211a3a823de5a8af6ca209b3aec95655
3urd Party <3urdparty@gmail.com>
2023-05-07 16:46:04 +0800

    Added Tests option

--- src/histoguzzy.cpp
+++ src/histoguzzy.cpp
@@ -112,7 +112,7 @@ void displayVerticalHistogram(vector<string>& headers, vector<float>& values) {
       int units = calculateNumberOfUnits(values[x], scale);
       // if the number of units needed to represent the bar is bigger than the
       // cutoff point for that y value
-      if (units >= cutoff) {
+      if (units >= cutoff - 1) {
         // It will output a barlet for that line
 
         cout << createRect(barWidth);
